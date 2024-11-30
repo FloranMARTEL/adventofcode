1f5d2b0624e9ff93a05078261d4d07569b0b69af
E226752U <floran.martel@etu.univ-nantes.fr>
2024-11-30 12:15:08 +0100

    init 2024

---
+++ 2024/01-day/parti2.py
@@ -0,0 +1,10 @@
+#parti2
+
+file = open("inputExemple.txt","r")
+#file = open("input.txt","r")
+data = file.read()
+
+datalist = data.split("\n")
+
+
+
---
+++ 2024/12-day/parti1.py
@@ -0,0 +1,10 @@
+#parti1
+
+file = open("inputExemple.txt","r")
+#file = open("input.txt","r")
+data = file.read()
+
+datalist = data.split("\n")
+
+
+
