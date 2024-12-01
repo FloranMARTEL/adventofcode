ae09d98ea2188626fcb5de5d941d18bbc0ac9643
E226752U <floran.martel@etu.univ-nantes.fr>
2024-12-01 06:23:54 +0100

    day1

--- 2024/01-day/parti1.py
+++ 2024/01-day/parti1.py
@@ -1,10 +1,36 @@
 #parti1
 
 file = open("inputExemple.txt","r")
-#file = open("input.txt","r")
+file = open("input.txt","r")
 data = file.read()
 
 datalist = data.split("\n")
+l1 = []
+l2 = []
+
+for i in range(len(datalist)):
+    dd = datalist[i].split("   ")
+    l1.append(int(dd[0]))
+    l2.append(int(dd[1]))
 
 
 
+
+
+
+l1.sort()
+l2.sort()
+
+
+
+
+print(l1)
+print(l2)
+
+diff = 0
+
+for i in range(len(l1)):
+    diff += abs(l1[i]-l2[i])
+    #print(abs(l1[i]-l2[i]))
+
+print(diff)
--- 2024/01-day/parti2.py
+++ 2024/01-day/parti2.py
@@ -1,10 +1,39 @@
 #parti2
 
+
 file = open("inputExemple.txt","r")
-#file = open("input.txt","r")
+file = open("input.txt","r")
 data = file.read()
 
 datalist = data.split("\n")
+l1 = []
+l2 = []
+
+for i in range(len(datalist)):
+    dd = datalist[i].split("   ")
+    l1.append(int(dd[0]))
+    l2.append(int(dd[1]))
 
 
 
+cptd = dict()
+
+for v in l2:
+
+    if v in cptd:
+        cptd[v] += 1
+    else:
+        cptd[v] = 1
+
+score = 0
+for v in l1:
+    mu = 0
+    if v in cptd:
+        mu = cptd[v]
+
+    score+= v*mu
+     
+
+
+
+print(score)
