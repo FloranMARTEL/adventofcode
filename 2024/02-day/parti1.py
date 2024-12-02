4fccd27aab432087628adacebead15b6e3746dc1
E226752U <floran.martel@etu.univ-nantes.fr>
2024-12-02 08:06:18 +0100

    day2 2024

--- 2024/02-day/parti1.py
+++ 2024/02-day/parti1.py
@@ -1,10 +1,32 @@
 #parti1
 
 file = open("inputExemple.txt","r")
-#file = open("input.txt","r")
+file = open("input.txt","r")
 data = file.read()
 
 datalist = data.split("\n")
 
 
+cpt = 0
+for ligne in datalist:
+
+    l = [int(a) for a in ligne.split(" ")]
+    inf = True
+    aug = True
+    dif = True
+    for i in range(len(l)-1):
+        if l[i] <= l[i+1]:
+            inf = False
+
+        if l[i] >= l[i+1]:
+            aug = False
+
+        if not(1 <= abs(l[i] - l[i+1]) <= 3):
+            dif = False
+    
+    if (inf or aug) and dif:
+        cpt+=1
+
+
+print(cpt)
 
--- 2024/02-day/parti2.py
+++ 2024/02-day/parti2.py
@@ -1,10 +1,44 @@
 #parti2
+from copy import deepcopy
 
 file = open("inputExemple.txt","r")
-#file = open("input.txt","r")
+file = open("input.txt","r")
+file = open("t","r")
 data = file.read()
 
 datalist = data.split("\n")
 
 
+cpt = 0
+for ligne in datalist:
 
+    l = [int(a) for a in ligne.split(" ")]
+    sec = False
+
+    for o in range(len(l)+1):
+        
+        l2 = deepcopy(l)
+        if o != len(l):
+            del l2[o]
+
+        inf = True
+        aug = True
+        dif = True
+        for i in range(len(l2)-1):
+            if l2[i] <= l2[i+1]:
+                inf = False
+
+            if l2[i] >= l2[i+1]:
+                aug = False
+
+            if not(1 <= abs(l2[i] - l2[i+1]) <= 3):
+                dif = False
+        
+        if (inf or aug) and dif:
+            sec = True
+    
+    if sec:
+        cpt+=1
+
+
+print(cpt)
