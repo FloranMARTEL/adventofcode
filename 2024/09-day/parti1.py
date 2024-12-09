8255ecbc393d18fd17532a7db4b8478c38867ecd
E226752U <floran.martel@etu.univ-nantes.fr>
2024-12-09 11:17:20 +0100

    day 9 2024

--- 2024/09-day/parti1.py
+++ 2024/09-day/parti1.py
@@ -1,10 +1,49 @@
 #parti1
 
 file = open("inputExemple.txt","r")
-#file = open("input.txt","r")
+file = open("input.txt","r")
 data = file.read()
 
-datalist = data.split("\n")
+# datalist = data.split("\n")
+
+data = list(map(int,data))
+stock = []
+num = 0
+for i in range(len(data)):
+
+    if i%2 == 0:
+
+        for j in range(data[i]):
+            stock.append(num)
+        num+=1
+    
+    else:
+        for j in range(data[i]):
+            stock.append(None)
 
 
+print(stock)
 
+
+i = 0
+while i < len(stock):
+
+    if stock[i] == None:
+        j = len(stock)-1
+        while stock[j] == None:
+            del stock[j]
+            j-=1
+
+        stock[i] = stock[j]
+        del stock[j]
+
+    i+=1
+
+print(stock)
+
+s = 0
+
+for i,v in enumerate(stock):
+    s += i*v
+
+print(s)
--- 2024/09-day/parti2.py
+++ 2024/09-day/parti2.py
@@ -1,10 +1,76 @@
 #parti2
 
+
 file = open("inputExemple.txt","r")
-#file = open("input.txt","r")
+file = open("input.txt","r")
 data = file.read()
 
-datalist = data.split("\n")
+# datalist = data.split("\n")
+
+data = list(map(int,data))
+stock = []
+num = 0
+for i in range(len(data)):
+
+    if i%2 == 0:
+
+        for j in range(data[i]):
+            stock.append(num)
+        num+=1
+    
+    else:
+        for j in range(data[i]):
+            stock.append(None)
 
 
+j = len(stock)-1
+while j >=0:
+    print(j)
+    if stock[j] == None:
+        pass
+    else:
+        l = data[stock[j]*2]
+
+        val = stock[j]
+        if val == 2:
+            print('ici')
+        elif val == 7:
+            print('ici')
+        
+
+        cpt = 0
+        find = False
+        for i in range(j):
+            if stock[i] != None:
+                cpt = 0
+            else:
+                cpt+=1
+                if cpt == l:
+                    find=True
+                    break
+            
+        if find:
+        
+            for o in range(i,i-l,-1):
+                stock[o] = val
+            
+            for o in range(j,j-l,-1):
+                stock[o] = None
+        else:
+            j-=l-1
+
+    j-=1
+    
+
+
+print(stock)
+
+s = 0
+
+for i,v in enumerate(stock):
+    if v != None:
+        s += i*v
+
+
+print(s)
 
