b06a2f4c805ff757d4c2e28553d24988d1108f1c
E226752U <floran.martel@etu.univ-nantes.fr>
2024-12-11 06:14:35 +0100

    o

--- 2024/11-day/parti1.py
+++ 2024/11-day/parti1.py
@@ -11,21 +11,24 @@ datalist = data.split(" ")
 
 i = 25
 while i > 0:
-
-    for j in range(len(datalist)):
+    j = 0
+    while j < len(datalist):
         if datalist[j] == "0":
             datalist[j] = "1"
         elif len(datalist[j])%2 == 0:
             p = datalist[j]
-            p1 = p[len(p)//2]
+            p1 = p[:len(p)//2]
             p2 = p[len(p)//2:]
             p2 = str(int(p2))
 
             del datalist[j]
             datalist.insert(j,p2)
             datalist.insert(j,p1)
+            j+=1
         else:
             datalist[j] = str(int(datalist[j]) * 2024)
+        
+        j+=1
 
     i-=1
 
