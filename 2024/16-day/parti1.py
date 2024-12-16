e1b01b0c41fe3e6d1a562a6a34254198ac5cd1cd
E226752U <floran.martel@etu.univ-nantes.fr>
2024-12-16 07:26:36 +0100

    dij

--- 2024/16-day/parti1.py
+++ 2024/16-day/parti1.py
@@ -1,10 +1,119 @@
 #parti1
 
+from copy import deepcopy
+
 file = open("inputExemple.txt","r")
-#file = open("input.txt","r")
+file = open("input.txt","r")
 data = file.read()
 
 datalist = data.split("\n")
 
 
+def dij(graph,start,end):
 
+    view = {start}
+    tab = {start : 0}
+    parent = {start : None}
+
+    candidat = None
+
+    while candidat == None or len(candidat) != 0:
+
+    
+
+        candidat = []
+
+        for v in view:
+            des = graph[v]
+            for key in des:
+                candidat.append((v,key,des[key]))
+        
+        for c in candidat:
+            if c[1] not in tab:
+                tab[c[1]] = tab[c[0]]+c[2]
+                parent[c[1]] = c[0]
+            
+            elif tab[c[1]] > tab[c[0]]+c[2]:
+                tab[c[1]] = tab[c[0]]+c[2]
+                parent[c[1]] = c[0]
+
+            del graph[c[0]][c[1]]
+
+            view.add(c[1])
+
+
+    path = [end]
+    curent = end
+    while curent != start:
+        curent = parent[curent]
+        path.append(curent)
+    path =  list(reversed(path))
+    return path
+    print(path)
+    
+
+
+# g = { (1,1,(-1,0)) : {(0,1,(-1,0)) : 1},
+#      (0,1,(-1,0)) : {(-1,1,(-1,0)) : 1}}
+        
+graph={
+  's':{'a':8,'b':4},
+  'a':{'b':4},
+  'b':{'a':3,'c':2,'d':5},
+  'c':{'d':2},
+  'd':{}
+}
+
+# r = dij(graph,'s','b')
+
+
+
+
+
+direction =((1,0),(0,1),(-1,0),(0,-1))
+
+di = dict()
+
+source = (None,None)
+end = (None,None)
+
+for i in range(len(datalist)):
+    for j in range(len(datalist[i])):
+
+        if datalist[i][j] == "S":
+            source = (i,j,direction[1])
+        if datalist[i][j] == "E":
+            end = (i,j,direction[2])
+
+        if datalist[i][j] != "#":
+            for idd in range(len(direction)):
+
+                d = direction[idd]
+                
+                di[(i,j,d)] = dict()
+                if i+d[0] >= 0 and i+d[0] < len(datalist) and j+d[1] >= 0 and j+d[1] < len(datalist[i]):
+                    if datalist[i+d[0]][j+d[1]] != "#":
+                        di[(i,j,d)][(i+d[0],j+d[1],d)] = 1
+                
+                di[(i,j,d)][(i,j,direction[(idd+1)%4])] = 1000
+                di[(i,j,d)][(i,j,direction[(idd-1)%4])] = 1000
+
+
+
+
+
+
+res = dij(deepcopy(di),source,end)
+res2 = dij(deepcopy(di),source,(end[0],end[1],direction[1]))
+
+su = 0
+for i in range(len(res)-1):
+    su += di[res[i]][res[i+1]]
+
+print(su)
+
+su = 0
+for i in range(len(res2)-1):
+    su += di[res2[i]][res2[i+1]]
+
+print(su)
