947d33630216cdf9f9bc261e5e162a655c83d641
E226752U <floran.martel@etu.univ-nantes.fr>
2024-12-19 06:21:44 +0100

    boujour

--- 2024/19-day/parti1.py
+++ 2024/19-day/parti1.py
@@ -1,10 +1,44 @@
 #parti1
 
+from  functools import *
+
 file = open("inputExemple.txt","r")
-#file = open("input.txt","r")
+file = open("input.txt","r")
 data = file.read()
 
-datalist = data.split("\n")
+datalist = data.split("\n\n")
 
+
+myset = datalist[0].split(",")
+
+myset = list(map(lambda x : x.replace(" ",""),myset))
+
+obj = datalist[1].split("\n")
+
+print(obj)
+
+@cache
+def check(ini, obj):
+    if ini == obj:
+        return True
+    if len(ini) > len(obj):
+        return False
+
+    res = False
+    for s in myset:
+        if obj.startswith(ini+s):
+            res = res or check(ini+s,obj)
+    
+    return res
+
+cpt = 0
+i = 0
+for o in obj:
+    print(i)
+    i+=1
+    if check("",o):
+        cpt +=1
+
+print(cpt)
 
 
--- 2024/19-day/parti2.py
+++ 2024/19-day/parti2.py
@@ -1,10 +1,41 @@
 #parti2
 
+from  functools import *
+
 file = open("inputExemple.txt","r")
-#file = open("input.txt","r")
+file = open("input.txt","r")
 data = file.read()
 
-datalist = data.split("\n")
+datalist = data.split("\n\n")
 
 
+myset = datalist[0].split(",")
 
+myset = list(map(lambda x : x.replace(" ",""),myset))
+
+obj = datalist[1].split("\n")
+
+print(obj)
+
+@cache
+def check(ini, obj):
+    if ini == obj:
+        return 1
+    if len(ini) > len(obj):
+        return False
+
+    res = 0
+    for s in myset:
+        if obj.startswith(ini+s):
+            res = res + check(ini+s,obj)
+    
+    return res
+
+cpt = 0
+i = 0
+for o in obj:
+    print(i)
+    i+=1
+    cpt += check("",o)
+
+print(cpt)
