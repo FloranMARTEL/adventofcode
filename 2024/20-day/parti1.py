bd5f841b7c0bd50c9af3202538c8ce1c086c8054
Floran MARTEL <floran.martel@gmail.com>
2024-12-20 17:58:05 +0100

    new

--- 2024/20-day/parti1.py
+++ 2024/20-day/parti1.py
@@ -1,7 +1,7 @@
 #parti1
 
 file = open("inputExemple.txt","r")
-file = open("input.txt","r")
+# file = open("input.txt","r")
 data = file.read()
 
 datalist = data.split("\n")
@@ -89,16 +89,18 @@ def moddij(graph,start):
                 candidat.append((cc,key,des[key]))
 
         for c in candidat:
-            if c[1] not in tab:
-                tab[c[1]] = tab[c[0]]+c[2]
-                parent[c[1]] = c[0]
-            
-            elif tab[c[1]] > tab[c[0]]+c[2]:
-                tab[c[1]] = tab[c[0]]+c[2]
-                parent[c[1]] = c[0]
+            if tab[c[0]]+c[2] < 21:
+
+                if c[1] not in tab:
+                    tab[c[1]] = tab[c[0]]+c[2]
+                    parent[c[1]] = c[0]
+                
+                elif tab[c[1]] > tab[c[0]]+c[2]:
+                    tab[c[1]] = tab[c[0]]+c[2]
+                    parent[c[1]] = c[0]
 
 
-            view.add(c[1])
+                view.add(c[1])
 
         min = None
         e = None
@@ -112,7 +114,7 @@ def moddij(graph,start):
             sew.add(e)
             cc = e
         else:
-            print("erreur")
+            # print("erreur")
             break
     
     return parent
@@ -185,50 +187,56 @@ print(pa)
 print(len(pa))
 from tqdm import tqdm
 cpt = 0
+
+memogra = dict()
 for ip in tqdm(range(len(pa)-2)):
-    par = []
 
-    for d in dir:
-
-        newi = pa[ip][0]+d[0]
-        newj = pa[ip][1]+d[1]
-
-        if newi >= 0 and newi < len(datalist) and newj >= 0 and newj < len(datalist[i]):
-            if datalist[newi][newj] == "#":
-                par.append(((newi,newj),moddij(grapvide,(newi,newj))))
-
-
+    found = False
     for jp in range(ip+2,len(pa)):
 
         if pa[jp][0] == 7 and pa[jp][1] == 3:
-            print("ici")
+            # print("ici")
+            pass
 
         a = pa[ip]
         b = pa[jp]
 
-        for p in par:
-            chepath = findpath(p[1],p[0],pa[jp])
-            if chepath != None and len(chepath) <= 20:
-                m = (jp-ip-1)-len(chepath)
-                co = len(pa)-(jp-ip-1) + len(chepath)
+        m = 0
+        for d in dir:
 
-                if 100-1 <= (jp-ip-1)-len(chepath):
-                    cpt+=1
-                    break
+            newi = pa[ip][0]+d[0]
+            newj = pa[ip][1]+d[1]
 
+            if newi >= 0 and newi < len(datalist) and newj >= 0 and newj < len(datalist[i]):
+                if datalist[newi][newj] == "#":
+                    
+                    if (newi,newj) not in memogra:
+                        p = ((newi,newj),moddij(grapvide,(newi,newj)))
+                        memogra[(newi,newj)] = p
+                    else:
+                        p = memogra[(newi,newj)]
+
+
+                    chepath = findpath(p[1],p[0],pa[jp])
+                    if chepath != None and len(chepath) <= 20:
+                        ma = (jp-ip)-len(chepath)
+                        co = len(pa)-1-ma
+
+                        if ma > m:
+                            m = ma
+
+                        # print(m)
+
+        if 72 == m:#100
+            # print((jp-ip)-len(chepath))
+            found = True
+            cpt+=1
         
-        # difr = abs(pa[ip][0] - pa[jp][0])
-        # difc = abs(pa[ip][1] - pa[jp][1])
+        
+                
+        
 
-        # su = difr+difc
 
-        # if su <= 20: #(difr == 2 and difc == 0 ) or (difc == 2 and difr == 0 ):
-            
-        #     m = jp-ip
-        #     co = len(pa)-(jp-ip-1) + 1
-
-        #     if 76 == (jp-ip-1)-1:
-        #         cpt+=1
 
 
 print(cpt)
