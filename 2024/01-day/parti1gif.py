cdd00044a86c52b9f54e93e4ae8992b53857120c
Floran MARTEL <floran.martel@gmail.com>
2024-12-26 17:49:16 +0100

    gifd1p1

--- 2024/01-day/parti1gif.py
+++ 2024/01-day/parti1gif.py
@@ -2,16 +2,14 @@
 from PIL import ImageDraw,Image,ImageFont
 import os
 
-import os, shutil
-folder = 'imgGif'
-for filename in os.listdir(folder):
-    file_path = os.path.join(folder, filename)
-    if os.path.isfile(file_path) or os.path.islink(file_path):
-        os.unlink(file_path)
+from Gif import *
+
+
+G = GifMaker(r'Source_Code_Pro\SourceCodePro-VariableFont_wght.ttf',48,(600,1000))
 
 
 file = open("inputExemple.txt","r")
-# file = open("input.txt","r")
+file = open("input.txt","r")
 data = file.read()
 
 datalist = data.split("\n")
@@ -23,70 +21,91 @@ for i in range(len(datalist)):
     l1[i] = int(dd[0])
     l2[i] = int(dd[1])
 
-size = (100+(len(str(max(l1)))*5*2),1000)
-space = 15
-lenfont = 5
-padding = 10
-
 ##
 
-im =Image.new("RGB",(size[0]+100,size[1]),(0,0,0))
+paddinTop = 50
 
-draw = ImageDraw.Draw(im)
+G.crate_image()
+G.writeText((10,10),"L1",fontsize=30)
+strl1 = list(map(str,l1))
+G.writeText((10,paddinTop),"\n".join(strl1))
 
-for i in range(len(l1)):
-    draw.multiline_text((padding,padding+space*i),str(l1[i]))
-    draw.multiline_text((size[0]-len(str(max(l1)))*lenfont-15,padding+space*i),str(l2[i]))
+G.writeText((10,10),"L2",False,fontsize=30)
+strl2 = list(map(str,l2))
+G.writeText((30*4,paddinTop),"\n".join(strl2),False)
+G.checkImage(1000)
 
-im.save("imgGif/im0001.png")
 
 l1.sort()
 l2.sort()
 
-im =Image.new("RGB",size,(0,0,0))
+G.crate_image()
 
-draw = ImageDraw.Draw(im)
+G.writeText((10,10),"L1",fontsize=30)
+strl1 = list(map(str,l1))
+G.writeText((10,paddinTop),"\n".join(strl1))
 
-for i in range(len(l1)):
-    draw.multiline_text((padding,padding+space*i),str(l1[i]))
-    draw.multiline_text((size[0]-len(str(max(l1)))*lenfont-15,padding+space*i),str(l2[i]))
-
-im.save("imgGif/im0002.png")
-
+G.writeText((10,10),"L2",False,fontsize=30)
+strl2 = list(map(str,l2))
+G.writeText((30*4,paddinTop),"\n".join(strl2),False)
+G.checkImage(1000)
 
 
+histdiff = [] 
 diff = 0
 
-for i in range(len(l1)):
+G.writeText(((G.size[0]//2),10),"diff",False,fontsize=30)
+from tqdm import tqdm
+for i in tqdm(range(len(l1))):
     diff += abs(l1[i]-l2[i])
 
-    p = 10+((size[0]-len(str(max(l1)))*5-15)-10)/2
+    histdiff.append(abs(l1[i]-l2[i]))
 
-    draw.multiline_text((p,10+15*i),str(diff))
+    if i < 10:
+        strhistdiff = list(map(str,histdiff))
+        G.writeText(((G.size[0]//2)-15,paddinTop),"\n".join(strhistdiff),True)
+        G.checkImage(3)
 
-    im.save(f"imgGif/im{(str(i+3)).zfill(4)}.png")
+    elif i%15  == 0 or i == len(l1)-1:
+        G.crate_image()
 
-    if i > 100:
-        break
+        G.writeText((10,10),"L1",fontsize=30)
+        strl1 = list(map(str,l1[i-5:]))
+        G.writeText((10,paddinTop),"\n".join(strl1))
 
+        G.writeText((10,10),"L2",False,fontsize=30)
+        strl2 = list(map(str,l2[i-5:]))
+        G.writeText((30*4,paddinTop),"\n".join(strl2),False)
+
+        G.writeText(((G.size[0]//2),10),"diff",False,fontsize=30)
+        strhistdiff = list(map(str,histdiff[i-5:]))
+        G.writeText(((G.size[0]//2)-30,paddinTop),"\n".join(strhistdiff),True)
+        G.checkImage()
+    
     
 
 
-print(diff)
+    
 
-import glob
-import contextlib
-# from PIL import Image
+G.writeText((10,G.size[1]-60),f'sum(diff) = {diff}',color=(0,255,0))
 
-with contextlib.ExitStack() as stack:
+G.checkImage(2000)
 
-    # lazily load images
-    imgs = (stack.enter_context(Image.open(f))
-            for f in sorted(glob.glob("imgGif/im*")))
+G.creatGIF("gifday1p1")
 
-    # extract  first image from iterator
-    img = next(imgs)
+# import glob
+# import contextlib
+# # from PIL import Image
 
-    # https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html#gif
-    img.save(fp="test.gif", format='GIF', append_images=imgs,
-             save_all=True, duration=200, loop=0)
+# with contextlib.ExitStack() as stack:
+
+#     # lazily load images
+#     imgs = (stack.enter_context(Image.open(f))
+#             for f in sorted(glob.glob("imgGif/im*")))
+
+#     # extract  first image from iterator
+#     img = next(imgs)
+
+#     # https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html#gif
+#     img.save(fp="test.gif", format='GIF', append_images=imgs,
+#              save_all=True, duration=200, loop=0)
